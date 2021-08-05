8e0102133a7e64eb8d93d7337f5c0c79f8bf43fb
Tim I Hely <seifertim@gmail.com>
2021-08-05 15:12:37 -0500

    Update FlxAtlasFrames.hx

--- flixel/graphics/frames/FlxAtlasFrames.hx
+++ flixel/graphics/frames/FlxAtlasFrames.hx
@@ -228,9 +228,9 @@ class FlxAtlasFrames extends FlxFramesCollection
 	 *                        Or you can just pass a path to the XML file in the assets directory.
 	 * @return  Newly created `FlxAtlasFrames` collection.
 	 */
-	public static function fromSparrow(Source:FlxGraphicAsset, Description:String):FlxAtlasFrames
+	public static function fromSparrow(Source:FlxGraphicAsset, Description:String, ?Unique:false):FlxAtlasFrames
 	{
-		var graphic:FlxGraphic = FlxG.bitmap.add(Source);
+		var graphic:FlxGraphic = FlxG.bitmap.add(Source, Unique);
 		if (graphic == null)
 			return null;
 
